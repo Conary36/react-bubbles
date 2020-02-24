4456cf2d04e542db514db1c760d2f35b56471a2f
Conary36 <cbeckford144@gmail.com>
2020-02-23 23:23:02 -0500

    functional edit/put/delete/add

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import ColorList from "./components/ColorList"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 import ProtectedRoute from "./utilities/ProtectedRoute"
 import BubblePage from "./components/BubblePage"
 
 const App = () => {
-  const [savedBubble, setSavedBubble] = useState([]);
-
-  const addToSavedBubbles = bloop => {
-    setSavedBubble([...savedBubble, bloop]);
-  }
+ 
 
   return (
     <Router>
       <div className="App">
-        <ColorList list={savedBubble}/>
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
 
-        <ProtectedRoute exact path='/BubblePage' component={BubblePage} />
+          <ProtectedRoute exact path='/BubblePage' component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
